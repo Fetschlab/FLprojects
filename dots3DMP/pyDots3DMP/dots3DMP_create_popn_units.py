22809bc61c9c281755089b55662fb5afe86b7a23
Steven Jerjian <sjerjian@jhu.edu>
2023-04-01 11:16:54 -0400

    add loop over sessions to main run

--- dots3DMP/pyDots3DMP/dots3DMP_create_popn_units.py
+++ dots3DMP/pyDots3DMP/dots3DMP_create_popn_units.py
@@ -21,7 +21,7 @@ class Neuron:
 
     nspks: int = field(init=False)
 
-    unit_id: int = field(default=0)
+    clus_id: int = field(default=0)
     clus_group: int = field(default=0)
     clus_label: str = field(default='none')
     ch_depth: tuple[int] = field(default=(0,1), 
@@ -54,6 +54,7 @@ class Population:
         - dictionary/pandas df of task events and times
     """
     rec_date: date
+    create_date: date = date.today().strftime("%Y%m%d")
     subject: str = 'test'
     set_num: int = 1
     pen_no: int = 1
@@ -61,10 +62,8 @@ class Population:
     grid_type: str = 'odd_sym'
 
     mdi_depth: int = field(default=0,metadata={'unit':'mm'})
-    area: str = 'MSTd'
-
     device: str = ''
-    numchs: int = 32
+    chs: list = field(default_factory=list, repr=False)
     
     sr: float = field(default=30000.0, metadata={'unit': 'Hz'})
 
@@ -76,30 +75,36 @@ class Population:
 
 # %%
 
-def build_rec_popn(subject, rec_date, set_num=1, probe_num=1):
+def build_rec_popn(subject, rec_date, sess_info, set_num=1, probe_num=1,
+                   groups2keep = ['good','mua'], 
+                   datapath = '/Volumes/homes/fetschlab/data/'):
     """
     Population is a class instance containing all simulataneously recorded neurons 
     (from one area/probe) and associated stimuli/behavioral events
 
     """
-    datapath='/Volumes/homes/fetschlab/data/'
-
     session = f'{subject}{rec_date}_{set_num}'
-    data_folder = PurePath(datapath, subject, f'{subject}_neuro/', str(rec_date))
     filepath = PurePath(data_folder,session)
-
-    # read info file
-    # need to decide how to handle rec_sets with more than one area, 
-    # and assign the area information / split the chs and units
-    sess_info_file = f'{subject}{rec_date}dots3DMP_info.mat'  
-    s = sio.loadmat(PurePath(data_folder,sess_info_file))
-    sess_info = s['info']
 
     # read in events for this set
     set_events_file = f'{subject}{rec_date}dots3DMPevents_{set_num}.mat'
     set_events_path = '/Users/stevenjerjian/Desktop/FetschLab/Analysis/data/lucio_neuro/rec_events'
     events = sio.loadmat(PurePath(set_events_path,set_events_file), simplify_cells=True)
     events = pd.DataFrame.from_dict(events['S'])
+
+    mdi_depth = np.mean(sess_info['depths'][sess_info['rec_group']==set_num])
+    paradigms = sess_info['par'][sess_info['rec_group']==set_num]
+    
+    if sess_info['gridxy'].ndim == 1:
+            gridxy = sess_info['gridxy']
+            gridtype = sess_info['gridtype']
+            device = sess_info['probe_ID']
+            chs = sess_info['chanlist']
+    else:
+        gridxy = sess_info['gridxy'][probe_num]
+        gridtype = sess_info['gridtype'][probe_num]
+        device = sess_info['probeID'][probe_num]
+        chs = sess_info['chanlist'][probe_num]        
                           
     # read in cluster groups (from manual curation)
     cgs = pd.read_csv(PurePath(filepath,'cluster_group.tsv'),sep='\t')
@@ -113,31 +118,36 @@ def build_rec_popn(subject, rec_date, set_num=1, probe_num=1):
     sa = np.squeeze(np.load(PurePath(filepath,'amplitudes.npy')))
 
     # initialize the neural population
-    rec_popn = Population(subject=subject,rec_date=rec_date, set_num=set_num, pen_no=sess_info['pen']
-                           events=events)
+    rec_popn = Population(subject=subject,rec_date=rec_date, set_num=set_num, pen_no=sess_info['pen'],
+                          grid_xy=gridxy, grid_type=gridtype, device=device, chs=chs, events=events)
     
-    for clus in range(len(cgs)):
+
+    # only go through clusters in this group of chs (i.e. one probe/area)
+    these_clus_ids = clus_info.loc[clus_info['ch'].isin(chs),'cluster_id']
+    cgs = cgs.loc[cgs['cluster_id'].isin(these_clus_ids) & cgs['group'].isin(groups2keep),:]
+    
+    for clus in cgs.itertuples():
 
         # get info for this cluster
-        unit_id = cgs['cluster_id'][clus]
-        unit_info = clus_info[clus_info['cluster_id']==unit_id].to_dict('records')[0]
-        label = unit_info['group']
+        clus_id = clus.cluster_id
+        unit_info = clus_info[clus_info['cluster_id']==clus_id].to_dict('records')[0]
 
         # create Neuron instance for this cluster
-        unit = Neuron(spiketimes=ss[sg==unit_id]/rec_set.sr, 
-                      amps=sa[sg==unit_id],
-                      unit_id=unit_id, clus_label=label,
-                      clus_group=get_cluster_group(label),
+        unit = Neuron(spiketimes=ss[sg==clus_id]/rec_popn.sr, 
+                      amps=sa[sg==clus_id],
+                      clus_id=clus_id, clus_label=clus.group,
+                      clus_group=get_cluster_group(clus.group),
                       ch_depth=(unit_info['ch'], unit_info['depth']),
-                      rec_date=rec_set.rec_date,set_num=rec_set.set_num)
+                      rec_date=rec_popn.rec_date,set_num=rec_popn.set_num)
         
-        # add it to the population
+        # ...and add it to the population
         rec_popn.units.append(unit)
 
-    return rec_set
+    return rec_popn
 
+# %% helpers
 
-def get_cluster_group(clus_label,labels=['NaN','mua','good','noise']):
+def get_cluster_group(clus_label, labels=['NaN','mua','good','noise']):
     """
     return numerical category for cluster type string label
     """
@@ -148,6 +158,41 @@ def get_cluster_group(clus_label,labels=['NaN','mua','good','noise']):
         return None
 
 
+def trial_psth(spiketimes, align_ev, trange, binsize=0.05, all_trials=False, normalize=False):
+    pass
+
+    nTr = align_ev.shape[0]
+
+    if nTr==0:
+        return
+    else:
+        
+        if align_ev.ndim==2:
+            align_ev = np.sort(align_ev,axis=1)
+            ev_order = np.argsort(align_ev,axis=1)
+            which_ev = ev_order[0] # align to this event
+        else:
+            align_ev[:,1] = align_ev[:,0]
+            which_ev = 0
+
+
+        tr_starts = align_ev[:,0] + trange[0]
+        tr_ends = align_ev[:,1] + trange[1]
+
+        durs = tr_ends - tr_starts
+
+        itr_start = 1
+        itr_end = nTr
+
+        if not all_trials and spiketimes.any():
+            itr_start = np.argmin(np.abs(tr_starts - spiketimes[0]))
+            itr_end = np.argmin(np.abs(tr_ends - spiketimes[-1]))
+        
+        
+
+
+
+
 
 
 
@@ -155,8 +200,36 @@ def get_cluster_group(clus_label,labels=['NaN','mua','good','noise']):
 
 if __name__ == '__main__':
 
+    #subject = input("Enter subject:")
 
-    rec_set = build_rec_popn('lucio', 20220512, set_num=1)
+    from dateutil.parser import parse
+
+    subject = 'lucio'
+    datapath='/Volumes/homes/fetschlab/data/'
+    data_folder = Path(datapath, subject, f'{subject}_neuro/')
+
+    # https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
+    rec_dates = [f.parts[-1] for f in data_folder.iterdir() 
+        if f.is_dir() and f.parts[-1][0:2]=='20']
+
+    for rec_date in rec_dates:
+        rec_folder = PurePath(datapath, subject, f'{subject}_neuro/', str(rec_date))
+
+       # read info file for this date
+        sess_info_file = f'{subject}{rec_date}dots3DMP_info.mat'  
+        s = sio.loadmat(PurePath(rec_folder,sess_info_file), simplify_cells=True)
+        sess_info = s['info']
+
+        if rec_date <= 20230330:
+            sess_info['numch'] = sess_info['chanlist'][-1]
+
+        nProbes = sess_info['gridxy'].ndim
+        nSets = np.max(sess_info['rec_group'])
+
+        for s in range(nSets):
+            for p in range(nProbes):
+                rec_popn = build_rec_popn('lucio', rec_date, sess_info,
+                                         set_num=s+1, probe_num=p)
 
 
     
