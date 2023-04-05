4bcb182a43b245cb5326db14b9511c916088b712
Steven Jerjian <sjerjian@jhu.edu>
2023-04-05 15:58:01 -0400

    pickle dump data

--- dots3DMP/pyDots3DMP/dots3DMP_build_population.py
+++ dots3DMP/pyDots3DMP/dots3DMP_build_population.py
@@ -6,15 +6,14 @@ from pathlib import Path, PurePath
 from datetime import date
 import scipy.io as sio
 from dataclasses import dataclass, field
-
-import pdb
-
+import pickle
 
 # %% define Neuron and Population classes
 
 
 @dataclass
 class Neuron:
+    
     spiketimes: np.ndarray = field(repr=False)
     amps: np.ndarray = field(repr=False)
 
@@ -32,7 +31,7 @@ class Neuron:
                                  metadata={'ord': ('contact number',
                                                    'contact_depth')})
     rec_date: date = date.today().strftime("%Y%m%d")
-    set_num: int = 1
+    rec_set: int = 1
 
     def __post_init__(self):
         self.nspks = len(self.spiketimes)
@@ -58,15 +57,16 @@ class Population:
         - list of unit instances, one per recorded unit
         - dictionary/pandas df of task events and times
     """
-    rec_date: date
+    
+    rec_date: date = date.today().strftime("%Y%m%d")
     create_date: date = date.today().strftime("%Y%m%d")
     subject: str = ''
     session: str = ''
-    set_num: int = 1
+    rec_set: int = 1
     probe_num: int = 1
     pen_num: int = 1
     grid_xy: tuple[int] = field(default=(np.nan, np.nan))
-    grid_type: str = 'odd_sym'
+    grid_type: str = ''
     area: str = ''
 
     mdi_depth: int = field(default=0, metadata={'unit': 'mm'})
@@ -83,76 +83,110 @@ class Population:
 # %% build Population class
 
 
-def build_rec_popn(subject, rec_date, set_info,
-                   groups2keep=['good', 'mua'],
-                   data_folder='/Volumes/homes/fetschlab/data/'):
+def build_rec_popn(subject, rec_date, rec_info, data, data_folder):
     """
-    Population is a class instance containing all simultaneous recorded units
-    (from one area/probe) and associated stimuli/behavioral events
+    Parameters
+    ----------
+    subject : TYPE
+        DESCRIPTION.
+    rec_date : TYPE
+        DESCRIPTION.
+    rec_info : TYPE
+        DESCRIPTION.
+    data : TYPE
+        DESCRIPTION.
+    data_folder : TYPE
+        DESCRIPTION.
 
-    rec_info - metadata
-    chs- chs for this population
+    Returns
+    -------
+    rec_popn : TYPE
+        DESCRIPTION.
 
     """
-    session = f"{subject}{rec_date}_{set_info['set_num']}"
+
+    session = f"{subject}{rec_date}_{rec_info['rec_set']}"
     filepath = PurePath(data_folder, session)
 
-    # read in events for this set
-    events_file = f"{subject}{rec_date}dots3DMPevents_{set_info['set_num']}.mat"
-    events = sio.loadmat(PurePath(data_folder, 'rec_events', events_file),
-                         simplify_cells=True)
-    events = pd.DataFrame.from_dict(events['S'])
+    # # read in cluster groups (from manual curation)
+    # cgs = pd.read_csv(PurePath(filepath, 'cluster_group.tsv'), sep='\t')
 
-    # read in cluster groups (from manual curation)
-    cgs = pd.read_csv(PurePath(filepath, 'cluster_group.tsv'), sep='\t')
+    # # read cluster info
+    # clus_info = pd.read_csv(PurePath(filepath, 'cluster_info.tsv'), sep='\t')
 
-    # read cluster info
-    clus_info = pd.read_csv(PurePath(filepath, 'cluster_info.tsv'), sep='\t')
+    # ss = np.squeeze(np.load(PurePath(filepath, 'spike_times.npy')))
+    # sg = np.squeeze(np.load(PurePath(filepath, 'spike_clusters.npy')))
+    # # st = np.squeeze(np.load(PurePath(filepath, 'spike_templates.npy')))
+    # sa = np.squeeze(np.load(PurePath(filepath, 'amplitudes.npy')))
 
-    ss = np.squeeze(np.load(PurePath(filepath, 'spike_times.npy')))
-    sg = np.squeeze(np.load(PurePath(filepath, 'spike_clusters.npy')))
-    # st = np.squeeze(np.load(PurePath(filepath, 'spike_templates.npy')))
-    sa = np.squeeze(np.load(PurePath(filepath, 'amplitudes.npy')))
+    events = {**data['events'], **data['pldaps']}
+    # events = pd.DataFrame({k: pd.Series(v) for k, v in events.items()})
+    events = pd.DataFrame.from_dict(events, orient='index')
+    events = events.T.convert_dtypes(infer_objects=True)
+    events['heading'].loc[np.abs(events['heading']) < 0.01] = 0
 
     # initialize the neural population
     rec_popn = Population(subject=subject.upper()[0], rec_date=rec_date,
-                          session=session, chs=set_info['chs'], events=events)
+                          session=session, chs=rec_info['chs'], events=events)
 
-    for key in set_info.keys():
-        vars(rec_popn)[key] = set_info[key]
+    # add all the metadata
+    for key in rec_info.keys():
+        vars(rec_popn)[key] = rec_info[key]
 
-    # only go through clusters in this group of chs (i.e. one probe/area)
-    these_clus_ids = clus_info.loc[clus_info['ch'].isin(set_info['chs']),
-                                   'cluster_id']
-    cgs = cgs.loc[cgs['cluster_id'].isin(these_clus_ids) &
-                  cgs['group'].isin(groups2keep), :]
-
-    for clus in cgs.itertuples():
-
-        # get info for this cluster
-        clus_id = clus.cluster_id
-        unit_info = clus_info[clus_info['cluster_id'] == clus_id].to_dict(
-            'records')[0]
-
-        # create Neuron instance for this cluster
-        unit = Neuron(spiketimes=ss[sg == clus_id]/rec_popn.sr,
-                      amps=sa[sg == clus_id],
-                      clus_id=clus_id, clus_label=clus.group,
-                      clus_group=get_cluster_group(clus.group),
-                      ch_depth=(unit_info['ch'], unit_info['depth']),
-                      rec_date=rec_popn.rec_date, set_num=rec_popn.set_num)
-
-        # ...and add it to the population
+    # add the units
+    nUnits = data['units']['cluster_id'].size
+    for u in range(nUnits):
+        unit = Neuron(spiketimes=data['units']['spiketimes'][u],
+                      amps=np.empty(data['units']['spiketimes'][u].shape),
+                      clus_id=data['units']['cluster_id'][u],
+                      clus_group=data['units']['cluster_type'],
+                      rec_date=rec_popn.rec_date, rec_set=rec_popn.rec_set)
         rec_popn.units.append(unit)
 
-    return rec_popn
+    # only go through clusters in this group of chs (i.e. one probe/area)
+    # these_clus_ids = clus_info.loc[clus_info['ch'].isin(rec_info['chs']),
+    #                                'cluster_id']
+    # cgs = cgs.loc[cgs['cluster_id'].isin(these_clus_ids) &
+    #               cgs['group'].isin(groups2keep), :]
 
-# %% helpers
+    # for clus in cgs.itertuples():
+
+    #     # get info for this cluster
+    #     clus_id = clus.cluster_id
+    #     unit_info = clus_info[clus_info['cluster_id'] == clus_id].to_dict(
+    #         'records')[0]
+
+    #     # create Neuron instance for this cluster
+    #     unit = Neuron(spiketimes=ss[sg == clus_id]/rec_popn.sr,
+    #                   amps=sa[sg == clus_id],
+    #                   clus_id=clus_id, clus_label=clus.group,
+    #                   clus_group=get_cluster_group(clus.group),
+    #                   ch_depth=(unit_info['ch'], unit_info['depth']),
+    #                   rec_date=rec_popn.rec_date, set_num=rec_popn.set_num)
+
+    #     # ...and add it to the population
+    #     rec_popn.units.append(unit)
+
+    return rec_popn, events
+
+# %%
 
 
 def get_cluster_group(clus_label, labels=['NaN', 'mua', 'good', 'noise']):
     """
-    return numerical category for cluster type string label
+
+    Parameters
+    ----------
+    clus_label : TYPE
+        DESCRIPTION.
+    labels : TYPE, optional
+        DESCRIPTION. The default is ['NaN', 'mua', 'good', 'noise'].
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
     """
 
     try:
@@ -161,65 +195,24 @@ def get_cluster_group(clus_label, labels=['NaN', 'mua', 'good', 'noise']):
         return None
 
 
-# def get_set_info(sess_info, set_num, probe_num, probe_nChs):
-
-#     rec_info = {}
-
-#     rec_info['set_num'] = set_num
-#     rec_info['probe_num'] = probe_num
-
-#     ch_inds = np.logical_and(sess_info['chanlist'] >= probe_nChs[probe_num],
-#                              sess_info['chanlist'] <= probe_nChs[probe_num+1])
-#     rec_info['chs'] = sess_info['chanlist'][ch_inds]
-#     rec_info['gridtype'] = sess_info['gridtype']
-
-#     # paradigms = sess_info['par'][sess_info['rec_group'] == set_num]
-
-#     if sess_info['numch'].size == 1:
-#         rec_info['grid_xy'] = sess_info['gridxy']
-#         rec_info['device'] = sess_info['probe_ID']
-#         rec_info['mdi_depth'] = np.mean(sess_info['depths']
-#                                         [sess_info['rec_group'] == set_num])
-#     else:
-#         gridxy = np.concatenate(sess_info['gridxy']).reshape(-1, 2)
-#         rec_info['grid_xy'] = gridxy[probe_num]
-#         rec_info['device'] = sess_info['probe_ID'][probe_num]
-#         rec_info['mdi_depth'] = np.mean(sess_info['depths'][probe_num]
-#                                         [sess_info['rec_group'] == set_num])
-
-#     return rec_info
-
-def get_set_info(sess_info):
-
-    rec_info = {}
-
-    rec_info['set_num'] = sess.REC_SET
-    rec_info['probe_num'] = sess.PROBE_NUM
-    rec_info['chs'] = np.arange(sess.MIN_CH, sess.MAX_CH+1)
-    rec_info['area'] = sess.BRAIN_AREA
-    rec_info['gt_depth_cm'] = sess.GT_DEPTH_CM
-    rec_info['pen_num'] = sess.PEN_NO_TOTAL
-    rec_info['mdi_depth'] = sess.MDI_DEPTH_UM
-    rec_info['device'] = (sess.PROBE_TYPE, sess.PROBE_ID)
-    rec_info['grid_xy'] = (sess.GRID_X, sess.GRID_Y)
-    rec_info['grid_type'] = sess.GRID_TYPE
-
-    return rec_info
-
 # %%
 
 
 if __name__ == '__main__':
 
-    import pickle
-    import pandas as pd
-    from collections import OrderedDict
-    from datetime import datetime
-
     # subject = input("Enter subject:")
     subject = 'lucio'
     datapath = '/Volumes/homes/fetschlab/data/'
     data_folder = Path(datapath, subject, f'{subject}_neuro/')
+    
+    mat_data_file = 'lucio_20220923-20230221_neuralData_PIVC.mat'
+    mat_folder = '/Users/stevenjerjian/Desktop/FetschLab/Analysis/data/lucio_neuro_datasets'
+
+    m = sio.loadmat(PurePath(mat_folder, mat_data_file),
+                    simplify_cells=True)
+    data = m['dataStruct']
+
+    
 
     # https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
     # rec_dates = [f.parts[-1] for f in data_folder.iterdir()
@@ -227,48 +220,47 @@ if __name__ == '__main__':
 
     rec_info_file = '/Users/stevenjerjian/Desktop/FetschLab/Analysis/RecSessionInfo.xlsx'
     rec_info = pd.read_excel(rec_info_file, sheet_name=subject.lower())
+    rec_info = rec_info.convert_dtypes(infer_objects=True)
+    rec_info.columns = rec_info.columns.str.lower()
 
-    rec_info['DATE'] = rec_info['DATE'].apply(lambda x:
+    rec_info['date'] = rec_info['date'].apply(lambda x:
                                               x.date().strftime('%Y%m%d'))
 
-    rec_data = OrderedDict()
+    rec_info.rename(columns={'mdi_depth_um': 'mdi_depth',
+                             'gt_depth_cm': 'gt_depth',
+                             'pen_no_total': 'pen_num',
+                             'brain_area': 'area'}, inplace=True)
 
-    for index, sess in rec_info.iterrows():
+    rec_df = pd.DataFrame.from_dict(rec_info)
+    rec_df['data'] = np.nan
+    
+    # TODO - decorator to create a column in rec_df for each paradigm
+    par = 'dots3DMP'
 
-        rec_date = sess['DATE']
-        print(rec_date)
+    for index, sess in enumerate(data):
 
-        rec_folder = PurePath(data_folder, str(rec_date))
+        # rec_date = sess['DATE']
+        rec_date = str(sess['date'])
+        set_num = sess['set']
+        print(rec_date, set_num)
 
+        rec_folder = PurePath(data_folder, rec_date)
 
-        set_info = get_set_info(sess)
+        idx = (rec_info['date'] == rec_date) & (rec_info['rec_set'] == set_num)
 
-# this is all unnecessary now
-        # read info file for this date
-        # sess_info_file = f'{subject}{rec_date}dots3DMP_info.mat'
-        # s = sio.loadmat(PurePath(rec_folder, sess_info_file),
-        #                 simplify_cells=True)
-        # sess_info = s['info']
+        rec_sess_info = rec_info.loc[idx, :].to_dict('records')[0]
+        rec_sess_info['chs'] = np.arange(rec_sess_info['min_ch'],
+                                         rec_sess_info['max_ch']+1)
+        rec_sess_info['grid_xy'] = (rec_sess_info['grid_x'],
+                                    rec_sess_info['grid_y'])
 
-        # if np.array(sess_info['chanlist']).size == 1:
-        #    continue
+        rec_popn, events = build_rec_popn(subject, rec_date, rec_sess_info,
+                                  sess['data'][par], data_folder=rec_folder)
 
-        # if datetime.strptime(rec_date, '%Y%m%d') <= datetime.strptime(
-        #       '20230330', '%Y%m%d'):
-        #    sess_info['numch'] = np.array(sess_info['chanlist'].size)
+        rec_df['data'][idx] = rec_popn
 
-        # nProbes = sess_info['gridxy'].ndim
-        # nSets = np.max(sess_info['rec_group'])
-
-        # probe_nChs = np.cumsum(sess_info['numch'])
-        # probe_nChs = np.insert(probe_nChs, 0, 0)
-
-        rec_popn = build_rec_popn(subject, rec_date, set_info,
-                                  data_folder=rec_folder)
-
-        # replace this with area reference?
-        key = f'{rec_popn.session}_p{rec_popn.probe_num}'
-        rec_data[key] = rec_popn
-
-    with open('alldata.pkl', 'wb') as file:
-        pickle.dump(rec_data, file)
+    with open('test_data.pkl', 'wb') as file:
+        pickle.dump(rec_df, file)
+        
+    # with open('test_data.pkl', 'rb') as file:
+    #    this_df = pickle.load(file)
