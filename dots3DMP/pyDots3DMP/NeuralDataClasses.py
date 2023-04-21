5310ab3294ec4b25b2d03a02dd7b58deb3269fd6
Steven Jerjian <sjerjian@jhu.edu>
2023-04-21 07:25:11 -0400

    add isi methods, and calc_firing_rates

--- dots3DMP/pyDots3DMP/NeuralDataClasses.py
+++ dots3DMP/pyDots3DMP/NeuralDataClasses.py
@@ -7,10 +7,11 @@ Created on Thu Apr 13 08:37:40 2023
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import date
 
 from dataclasses import dataclass, field
-from dots3DMP_FRutils import trial_psth, condition_averages
+from dots3DMP_FRutils import trial_psth
 
 # %%
 
@@ -23,20 +24,36 @@ class Unit:
     clus_id: int = field(default=0)
     rec_date: date = date.today().strftime("%Y%m%d")
     
+    def __len__(self):
+        return len(self.spiketimes)
+    
     def isi(self):
         return np.diff(self.spiketimes)
     
-    def isi_hist(self, binsize=0.02):
-        x = np.arange(0, 2, binsize)
-        return np.histogram(self.spiketimes, x)
-
-    # TODO post_init on waveforms
+    def isi_cv(self):
+        y = self.isi()
+        return np.std(y) / np.mean(y)
     
-    # TODO simple methods e.g. isi, autocorr, ifr
-    # TODO inter-unit methods e.g. cross-corr in spiketimes, similarity
-    # def ifr(self):
-    # def spike_acorr(self, binsize=0.02, lag=)
-    # def spike_xcorr(self, other, binsize=0.02, lag)
+    def isi_hist(self, binsize=0.02, max_isi=2):
+        x = np.arange(0, max_isi, binsize)
+        y = np.histogram(self.isi(), x)
+        return y, x
+    
+    def ifr(self):
+        return 1 / self.isi()
+    
+    def acf(self, binsize=0.02, maxlag=2):
+        ...
+        
+    def ccf(self, other, binsize=0.02, maxlag=2):
+        ...
+        
+    def mean_wf(self):
+        ...
+        
+        
+    #def summary(self, binsize=0.01, max=0.2, plot=False):
+    #  isi, corr, ifr, wf_width 2x2 subplots
 
 @dataclass
 class ksUnit(Unit):
@@ -64,16 +81,6 @@ class ksUnit(Unit):
 
     # TODO add contam pct?
 
-    # TODO fix these up, should they be properties??
-    @property
-    def trial_psth(self, align_ev, **kwargs):
-        return trial_psth(self.spiketimes, align_ev, **kwargs)
-
-    @property
-    def condition_psth(self, condlist, cond_groups=None):
-        return condition_averages(self.frates, condlist, cond_groups)
-        
-
     #def isi(self, binsize=0.01, max=0.2, plot=False): 
     # return isi hist, violations count
     #def corr(self, binsize=0.01, max=0.2, plot=False):
@@ -81,8 +88,31 @@ class ksUnit(Unit):
     #def plot_waveforms(self):  
     #def wf_width(self):
 
-    #def summary(self, binsize=0.01, max=0.2, plot=False):
-    #  isi, corr, ifr, wf_width 2x2 subplots
+        
+    def raster_plot(self, events, conds, align_ev, trange=np.array([-2, 2]),
+                    binsize=0.05, sm_params={}):
+        
+        good_trs = events['goodtrial'].to_numpy(dtype='bool')
+        condlist = events[conds].loc[good_trs, :]
+        
+        align_ev = events.loc[good_trs, align_ev].to_numpy(dtype='float64')
+
+        fr, x, spks = trial_psth(self.spiketimes, align_ev, trange, 
+                          binsize, sm_params)
+        
+        # will use matplotlib.eventplot!!
+        # sort spktimes according to trial number, or some ordering (e.g. grouped by condition)
+        
+        # time these two at some point
+        # list(np.array(a, dtype=object)[order])
+        # or [items[i] for i in order]
+        
+        
+        fig, ax = plt.subplots(1, 1)
+        # default is horizontal
+        ax.eventplot(spks, lineoffsets=list(range(len(spks))))
+        
+        return fr, x, spks
 
 
 @dataclass
@@ -113,3 +143,107 @@ class Population:
 
     units: list = field(default_factory=list, repr=False)
     events: dict = field(default_factory=dict, repr=False)
+    
+
+    def calc_firing_rates(self, align=['stimOn'], trange=np.array([[-2, 2]]),
+                          binsize=0.05, sm_params={},
+                          condlabels=['modality', 'coherence', 'heading'],
+                          clus_groups=[1, 2],
+                          return_Dataset=False):
+        """
+        
+
+        Parameters
+        ----------
+        align : nested lists, optional
+            DESCRIPTION. The default is ['stimOn'].
+        trange : nested lists, optional
+            DESCRIPTION. The default is np.array([[-2, 2]]).
+        binsize : FLOAT, optional
+            DESCRIPTION. The default is 0.05.
+        sm_params : DICT, optional
+            DESCRIPTION. The default is {}
+        condlabels : LIST, optional
+            DESCRIPTION. The default is ['modality', 'coherence', 'heading'].
+        clus_groups : LIST, optional
+            DESCRIPTION. The default is [1, 2].
+        return_Dataset : BOOL, optional
+            DESCRIPTION. The default is False.
+
+        Raises
+        ------
+        ValueError
+            If there are no 'good trials'
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+
+        # TODO somewhere need to get an update to tvec to be all relative to one event?
+        # so that we can plot on the same time axis if we want
+
+        good_trs = self.events['goodtrial'].to_numpy(dtype='bool')
+        condlist = self.events[condlabels].loc[good_trs, :]
+
+        rates = []
+        tvecs = []
+        #align_lst = []
+
+        for al, t_r in zip(align, trange):
+
+            if self.events.loc[good_trs, al].isna().all(axis=0).any():
+                raise ValueError(al)
+
+            align_ev = self.events.loc[good_trs, al].to_numpy(dtype='float64')
+
+            # get spike counts and relevant t_vec for each unit - thanks chatGPT!
+            # trial_psth in list comprehesnsion is going to generate a list of
+            # tuples, the zip(*iter) syntax allows us to unpack the tuples into
+            # separate variables
+            spike_counts, t_vec, _ = \
+                zip(*[(trial_psth(unit.spiketimes, align_ev, t_r, 
+                                  binsize, sm_params))
+                      for unit in self.units if unit.clus_group in clus_groups])
+
+            rates.append(np.asarray(spike_counts))
+            tvecs.append(np.asarray(t_vec[0]))
+            
+            # previously wanted dict with key as alignment event, but al[0]
+            # might not be unique!
+
+            #align_lst.append(np.asarray(list(repeat(al, len(t_vec[0])))))
+
+        #align_arr = np.concatenate(align_lst)
+        
+        if return_Dataset:
+            # now construct a dataset
+            arr = xr.DataArray(np.concatenate(rates, axis=2),
+                               coords=[unitlabels,
+                                       condlist.index,
+                                       np.concatenate(tvecs)],
+                               dims=['unit', 'trial', 'time'])
+
+            cond_coords = {condlabels[c]: ('trial', condlist.iloc[:, c])
+                           for c in range(len(condlist.columns))}
+
+            ds = xr.Dataset({'firing_rate': arr},
+                            coords={'align_event': ('time', align_arr),
+                                    **cond_coords})
+
+            ds.attrs['rec_date'] = self.rec_date
+            ds.attrs['area'] = self.area
+
+            return ds
+
+        else:
+            # return separate vars
+            return rates, tvecs, condlist
+        
+        
+
+
+# %% functions to run on an individual class instance 
+# (in essence class methods, so make them so)
