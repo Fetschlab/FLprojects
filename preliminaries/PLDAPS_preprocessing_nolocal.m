6876226667ca8989074675ed3141f62ab039582f
Steven Jerjian <sjerjian@jhu.edu>
2023-10-17 16:32:34 -0400

    preallocate data fields sizes for memory

--- preliminaries/PLDAPS_preprocessing_nolocal.m
+++ preliminaries/PLDAPS_preprocessing_nolocal.m
@@ -18,19 +18,21 @@ dateRange = 20220512:20230601;
 localDir = '/Users/stevenjerjian/Desktop/FetschLab/PLDAPS_data/';
 
 opts = struct('eyemovement', 1);
-data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDir, opts);
+data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDir, [], opts);
 
 filename = [subject '_' num2str(dateRange(1)) '-' num2str(dateRange(end)) '_wEM.mat'];
-save(fullfile(localDir, filename), 'data');
+save(fullfile(localDir, filename), 'data', 'analogs');
 
 
-function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDir, options)
+function [data] = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDir, data, options)
 
 
     % TODO consider providing existing file to append to...
 
-    if nargin < 5, options = struct(); end
+    if nargin < 6, options = struct(); end
     if nargin < 4, localDir = pwd; end
+
+    use_existing_file = ~(nargin < 5 || isempty(data));
 
     opt_fields = {'nexonar', 'dotposition', 'eyemovement', 'reward', 'eventtiming', ...
         'useSCP', 'useVPN'};
@@ -41,10 +43,8 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
         end
     end
 
-    if options.eyemovement || options.nexonar
-        options.eventtiming = 1;
-    end
-
+    options.eventtiming = options.eyemovement || options.nexonar;
+        
     if options.eventtiming
         pt_names = {'timeTargFixEntered', 'timeConfTargEntered', 'timeToConfidence'};
     
@@ -52,6 +52,9 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
             'timeMotionStateBegin', 'timeMotionDone', 'timeGoCue', ...
             'TimeTargEntered', 'motionStartTime', 'timeBreakFix', 'timeComplete', ...
             'delayToGoCue', 'delayToDots', 'holdAfterDotsOnset', 'timeLastFrame'};
+    else
+        pt_names = {};
+        st_names = {};
     end
     bhv_names = {'choice', 'RT', 'PDW', 'TargMissed', 'oneTargChoice', 'oneTargConf'};
 
@@ -72,11 +75,31 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
 
 
     % initialize data struct
-    data = struct;
-    data.filename = {};
-    data.subj = {};
-    data.date = [];
-    data.choice = []; % initialize this one field, to keep counter of numtrials
+    if ~use_existing_file
+        ntr_est = 3e5;
+        data.filename = cell(ntr_est, 1);
+        data.subj = cell(ntr_est, 1);
+        data.date = nan(ntr_est, 1);
+
+        all_flds = unique([bhv_names, pt_names, st_names, {'iTrial', 'trialNum'}]);
+       
+        for f = 1:length(all_flds)
+            data.(all_flds{f}) = nan(ntr_est, 1);
+            
+        end
+        if options.eyemovement
+            data.ADCdata = cell(ntr_est, 1);
+            data.ADCtime = cell(ntr_est, 1);
+        end
+
+        T = 0;
+
+    else
+        % use existing fields..NEED TO ADD CHECKS BELOW TO ONLY ADD NEW
+        % DATA IF FIELD EXISTS
+        all_flds = fieldnames(data);
+        T = find(isnan(data.date), 1, 'first');
+    end
 
 
     %% get data from NAS
@@ -84,7 +107,6 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
     % https://www.howtogeek.com/66776/how-to-remotely-copy-files-over-ssh-without-entering-your-password/
     
     % VERSION 3.0: 10-06-2023 SJ
-    % loading directly from mounted NAS each time might 
     
     % get file list from remote dir
     if ~options.useVPN
@@ -107,23 +129,21 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
     for f = 1:length(remoteFiles)
         thisDate = str2double(remoteFiles{f}(dateStart:dateStart+7)); % yyyymmdd date format
         thisPar  = remoteFiles{f}(dateStart+8:length(remoteFileNames{f})-4);
-%         thisPar  = remoteFiles{f}(dateStart+8:length(remoteFiles{f})-8);
 
         if ~any(strcmp(data.filename, remoteFileNames{f})) && any(dateRange == thisDate) && strcmp(thisPar, paradigm) 
             % ok, we want this file
-
-            T = length(data.choice);
 
             try
 
                 tstart = tic;
 
                 if options.useSCP
+
+                    % TODO - save reduced version of file, or delete afterwards?
                     cmd = ['scp -r ' ip_add ':' remoteDir remoteFiles{f} ' ' localDir];
                     system(cmd, "-echo")
                     load(fullfile(localDir, remoteFiles{f}), '-mat', 'PDS')
-                    % TODO, delete file afterwards, or save reduced
-                    % version...
+                    
                 else
                     load(fullfile(mountDir, remoteFiles{f}), '-mat', 'PDS');
                 end
@@ -143,74 +163,73 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
 
                 % extract desired fields for each trial, append to struct
                 for t = 1:length(PDS.data)
+                    
+                    if isfield(PDS.data{t}.behavior,'choice') % excluding trials with missing data
 
-
-                    if isfield(PDS.data{t}.behavior,'choice') % and save out the data, excluding trials with
-                        % missing data (choice is a good marker for this)
                         T = T+1; % increment trial counter
 
-                        data.trialNum(T,1) = t;
+                        data.trialNum(T) = t;
 
-                        data.filename{T,1} = remoteFileNames{f};
-                        data.date(T,1) = thisDate;
+                        data.filename{T} = remoteFileNames{f};
+                        data.date(T) = thisDate;
 
                         if contains(subject,'human')
-                            data.subj{T,1} = remoteFileNames{f}(dateStart(1)-3:dateStart(1)-1); % 3-letter code
+                            data.subj{T} = remoteFileNames{f}(dateStart(1)-3:dateStart(1)-1); % 3-letter code
                         else
-                            data.subj{T,1} = subject;
+                            data.subj{T} = subject;
                         end
 
                         % independent variables (conditions) are stored in PDS.conditions.stimulus
                         fnames = fieldnames(PDS.conditions{t}.stimulus);
                         for F = 1:length(fnames)
-                            data.(fnames{F})(T,1) = PDS.conditions{t}.stimulus.(fnames{F});
+                            data.(fnames{F})(T) = PDS.conditions{t}.stimulus.(fnames{F});
                         end
 
                         % dependent variables (outcomes) stored in PDS.data.behavior
                         for F = 1:length(bhv_names)
-                            data.(bhv_names{F})(T,1) = PDS.data{t}.behavior.(bhv_names{F});
+                            data.(bhv_names{F})(T) = PDS.data{t}.behavior.(bhv_names{F});
                         end
 
                         % misc
                         try
-                            data.iTrial(T,1) = PDS.conditions{t}.pldaps.iTrial;
+                            data.iTrial(T) = PDS.conditions{t}.pldaps.iTrial;
                         catch
-                            data.iTrial(T,1) = NaN;
+                            data.iTrial(T) = NaN;
                         end
                         
                         % options
                         if options.reward
                             fnames = fieldnames(PDS.data{t}.reward);
                             for F = 1:length(fnames)
-                                data.(fnames{F})(T,1) = PDS.data{t}.reward.(fnames{F});
+                                data.(fnames{F})(T) = PDS.data{t}.reward.(fnames{F});
                             end
                         end
 
                         if options.eyemovement
                             try
-                                data.ADCdata{T, 1} = PDS.data{t}.datapixx.adc.data;
+                                data.ADCdata{T} = PDS.data{t}.datapixx.adc.data;
                                 dp_time = PDS.data{t}.datapixx.unique_trial_time(2);
-                                data.ADCtime{T, 1} = PDS.data{t}.datapixx.adc.dataSampleTimes - dp_time;
+                                data.ADCtime{T} = PDS.data{t}.datapixx.adc.dataSampleTimes - dp_time;
                             catch
-                                data.ADCdata{T, 1} = NaN;
-                                data.ADCtime{T, 1} = NaN;
+                                data.ADCdata{T} = NaN;
+                                data.ADCtime{T} = NaN;
                             end
                         end
 
                         if options.eventtiming
                             for F = 1:length(pt_names)
                                 if isfield(PDS.data{t}.postTarget, pt_names{F})
-                                    data.(pt_names{F})(T,1) = PDS.data{t}.postTarget.(pt_names{F});
+                                    data.(pt_names{F})(T) = PDS.data{t}.postTarget.(pt_names{F});
                                 else
-                                    data.(pt_names{F})(T,1) = NaN;
+                                    data.(pt_names{F})(T) = NaN;
                                 end
                             end
 
                             for F = 1:length(st_names)
                                 if isfield(PDS.data{t}.stimulus, st_names{F})
-                                    data.(st_names{F})(T,1) = PDS.data{t}.stimulus.(st_names{F});
+                                    data.(st_names{F})(T) = PDS.data{t}.stimulus.(st_names{F});
                                 else
-                                    data.(st_names{F})(T,1) = NaN;
+                                    data.(st_names{F})(T) = NaN;
                                 end
                             end
                         end
@@ -223,16 +242,17 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
                                     PDS.data{t}.stimulus.dotZ_3D = dotZ_3D{t};
                                     PDS.data{t}.stimulus.dotSize = dotSize{t};
                                 end
-                                data.dotX_3D = PDS.data{t}.stimulus.dotX_3D;
-                                data.dotY_3D = PDS.data{t}.stimulus.dotY_3D;
-                                data.dotZ_3D = PDS.data{t}.stimulus.dotZ_3D;
-                                data.dotSize = PDS.data{t}.stimulus.dotSize;
+                                data.dotX_3D{T} = PDS.data{t}.stimulus.dotX_3D;
+                                data.dotY_3D{T} = PDS.data{t}.stimulus.dotY_3D;
+                                data.dotZ_3D{T} = PDS.data{t}.stimulus.dotZ_3D;
+                                data.dotSize{T} = PDS.data{t}.stimulus.dotSize;
                             catch
                             end
                         end
 
                         if options.nexonar
                             disp('not yet implemented')
+                            % need to move over from original
                         end
                 
 
@@ -254,6 +274,5 @@ function data = pds_preprocessing_dots3DMP(subject, paradigm, dateRange, localDi
     disp('done')
 end
         
-    
     
 
